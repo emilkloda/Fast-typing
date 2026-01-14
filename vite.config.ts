8dae46b005157db2a9049def960616dd1e13e573
emilkloda <e_kloda@wp.pl>
2026-01-14 19:18:01 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // lub inny plugin, którego używasz
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
