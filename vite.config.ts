e255ae36ea343cf6f407e6af1888cff757867158
emilkloda <e_kloda@wp.pl>
2026-01-14 18:49:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // lub inny plugin, którego używasz
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/nazwa-twojego-repozytorium/', // WAŻNE: musi zaczynać się i kończyć slashem
+})
